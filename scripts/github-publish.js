8d3c0d7a4b9a76d49d8171716b10b242ca1d90c8
danyo <dan@noemail.com>
2021-03-24 21:59:23 +1300

    uncomment deploy

--- scripts/github-publish.js
+++ scripts/github-publish.js
@@ -8,11 +8,11 @@ const version = require('../package.json')
 console.log('dist path', distPath)
 const branch = process.env.BRANCH || 'gh-pages.'
 console.log('branch', branch)
-// ghpages.publish(distPath, { tag: version.version, branch }, function (err) {
-//     if (err) {
-//         console.error('Failed to publish', err)
-//         process.exitCode = -1
-//     } else {
-//         console.log('publish success')
-//     }
-// })
+ghpages.publish(distPath, { tag: version.version, branch }, function (err) {
+    if (err) {
+        console.error('Failed to publish', err)
+        process.exitCode = -1
+    } else {
+        console.log('publish success')
+    }
+})
