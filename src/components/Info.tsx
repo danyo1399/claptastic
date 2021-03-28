0ef8a6b9562f15b1ecda81810de0915ff575a7a6
danyo <dan@noemail.com>
2021-03-28 22:01:12 +1300

    move url to the top

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect, HTMLAttributes } from 'react'
 import styled, { css, keyframes } from 'styled-components'
 
 const Container = styled.div`
-    position: fixed;
-    bottom: 20px;
-    left: 15px;
+    position: absolute;
+    top: 5px;
+    left: 10px;
     color: white;
     font-weight: 300;
+    @media (max-height: 300px) {
+        display: none;
+    }
 `
 
 export interface InfoProps extends HTMLAttributes<HTMLDivElement> {}
